023bd9da4ab1c7e084f01a264ed0a57b382cda9b
Saad Amir <saadamir070@gmail.com>
2025-08-20 16:33:00 +0500

    feat(notification): add notification module

---
+++ src/notifications/notifications.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationsService } from './notifications.service';
+import { NotificationsResolver } from './notifications.resolver';
+import { Notification } from './entities/notification.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Notification])],
+  providers: [NotificationsService, NotificationsResolver],
+  exports: [NotificationsService],
+})
+export class NotificationsModule {}
